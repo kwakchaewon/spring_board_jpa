5bb65157f2d549be2440efbb53f043c27c0773e3
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-11-27 20:19:11 +0900

    빈을 통한 PasswordEncoder 주입

--- bootboard/src/main/java/com/example/bootboard/user/UserService.java
+++ bootboard/src/main/java/com/example/bootboard/user/UserService.java
@@ -2,21 +2,26 @@ package com.example.bootboard.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public SiteUser create(String username, String email, String password){
         SiteUser user = new SiteUser();
         user.setUsername(username);
         user.setEmail(email);
 
-        // 암호화를 위해 시큐리티의 BCryptPasswordEncoder 사용
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        // 빈으로 등록 후 PasswordEncoder 주입받아 사용
         user.setPassword(passwordEncoder.encode(password));
+
+        // 암호화를 위해 시큐리티의 BCryptPasswordEncoder 사용
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
         return user;
     }
