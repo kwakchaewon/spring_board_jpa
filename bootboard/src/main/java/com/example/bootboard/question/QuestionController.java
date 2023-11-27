6742fd8fa41526cbefeed8a219d1b540ea75206b
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-11-27 09:15:22 +0900

    폼 클래스 검증을 통한 질문 등록 구현

--- bootboard/src/main/java/com/example/bootboard/question/QuestionController.java
+++ bootboard/src/main/java/com/example/bootboard/question/QuestionController.java
@@ -1,11 +1,11 @@
 package com.example.bootboard.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +29,41 @@ public class QuestionController {
         model.addAttribute("question",question);
         return "question_detail";
     }
+
+    @GetMapping("/create")
+    public String questionCreate(QuestionForm questionForm){
+        return "question_form";
+    }
+
+    /**
+     * 질문등록 컨트롤러
+     */
+//    @PostMapping("/create")
+//    public String questionCreate(@RequestParam String subject, @RequestParam String content){
+//        return "redirect:/question/list";
+//    }
+
+
+    /**
+     * 폼 & validation 적용 질문 등록 컨트롤러
+     * 매개변수 (subject, content) -> QuestionForm:
+     */
+    @PostMapping("/create")
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult){
+        
+        // 1. 에러가 있을 경우 다시 폼 작성
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+
+        //2. 에러가 없다면 질문 등록
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list";
+    }
+
+
+
+
+
+
 }
--- bootboard/src/main/java/com/example/bootboard/question/QuestionService.java
+++ bootboard/src/main/java/com/example/bootboard/question/QuestionService.java
@@ -3,6 +3,7 @@ package com.example.bootboard.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +24,13 @@ public class QuestionService {
         } else {
             throw new DataNotFoundException("question not found");
         }
+    }
+
+    public void create(String subject, String content){
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
     }
 }
