39012b872d602e7465fb4ed8d0baaf66f5ea75ae
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-02 00:03:05 +0900

    답변 삭제 기능 구현

--- bootboard/src/main/java/com/example/bootboard/answer/AnswerController.java
+++ bootboard/src/main/java/com/example/bootboard/answer/AnswerController.java
@@ -78,5 +78,19 @@ public class AnswerController {
         return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
     }
 
+    /**
+     * 답변 삭제 컨트롤러
+     */
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(Principal principal, @PathVariable("id") Integer id) {
+        Answer answer = this.answerService.getAnswer(id);
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.answerService.delete(answer);
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+    }
+
 
 }
