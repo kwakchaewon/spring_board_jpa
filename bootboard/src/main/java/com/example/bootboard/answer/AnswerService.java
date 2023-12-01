01e2a6ceb80b5b2ab89bd7a9dbeff80a4f4d33d2
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-02 00:01:01 +0900

    답변 수정일자 칼럼 추가 및 답변 수정 기능 구현

--- bootboard/src/main/java/com/example/bootboard/answer/AnswerService.java
+++ bootboard/src/main/java/com/example/bootboard/answer/AnswerService.java
@@ -1,5 +1,6 @@
 package com.example.bootboard.answer;
 
+import com.example.bootboard.question.DataNotFoundException;
 import com.example.bootboard.question.Question;
 import com.example.bootboard.question.QuestionRepository;
 import com.example.bootboard.user.SiteUser;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +21,21 @@ public class AnswerService {
         answer.setCreateDate(LocalDateTime.now());
         answer.setQuestion(question);
         answer.setAuthor(author);
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()){
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void update(Answer answer, String content){
+        answer.setContent(content);
+        answer.setUpdatedDate(LocalDateTime.now());
         this.answerRepository.save(answer);
     }
 
